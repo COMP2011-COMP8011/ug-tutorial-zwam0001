504bc37b3cbfceb9e6615b596eb793f8d976c069
Zwam0001 <nicolesamanthazwambila@gmail.com>
2026-08-05 02:33:12 +0000

    Completed all tasks of t02

--- tutorials/t02/src/components/PlayerProfile.jsx
+++ tutorials/t02/src/components/PlayerProfile.jsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+import { getPlayerRank } from '../utils./gamelogic.js';
+
 function PlayerProfile() {
-    
-const player = { username: "PixelPioneer", level: 5, active: true };
+
+//const player = { username: "PixelPioneer", level: 5, active: true };
+const [player, setPlayer] = useState({
+    username: "PixelPioneer",
+    level: 5,
+    active: true
+});
 const { username, level, active } = player;
 
+
+const handleLevelUp = () => {
+    const updatedPlayer = { ...player, level: level + 1 };
+    //console.log(“Updated player state: “, updatedPlayer);
+    setPlayer(updatedPlayer);
+
+};
+
+
 return (
-    <div className="profile-card">
-        <h2>Player: {username}</h2>
-        <p>Current Level: {level}</p>
-    </div>
+    <>
+         <div className="profile-card">
+             <h2>Player: {username}</h2>
+             <p>Rank: {getPlayerRank(level)}</p>
+             <button onClick={handleLevelUp}>Gain XP</button>
+             {active ? <p>Status: Online</p> : <p>Status: Offline</p>}
+
+         </div>
+    </>
 );
 }
 
