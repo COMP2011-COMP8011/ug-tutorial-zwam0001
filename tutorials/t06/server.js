0d5dad46c16d199a746ebfa15fd5a9457cc3f5a5
Zwam0001 <nicolesamanthazwambila@gmail.com>
2026-09-20 23:49:32 +0000

    task 2 complete

--- tutorials/t06/server.js
+++ tutorials/t06/server.js
@@ -1,12 +1,29 @@
-import express from 'express';
+import express from "express";
+const app = express();
+const PORT = 3000;
+ 
 app.use(express.json());
 
-const app = express();
 let students = [
-{id: 1, name: "Ada Lovelace", major: "Mathematics" },
-    {id: 2, name: "Alan Turing", major: "Computer Science"}
+  { id: 1, name: "Alice", major: "Math" },
+  { id: 2, name: "Bob", major: "Physics" },
+  { id: 3, name: "Chloe", major: "Math" },
+  { id: 4, name: "Dev", major: "Biology" },
 ];
 
-
-const PORT = 5100;
-app.listen(PORT);
+app.get("/api/students", (req, res) => {
+  if (req.query.major) {
+    const filtered = students.filter((st) => st.major === req.query.major);
+    return res.status(200).json(filtered);
+  }
+  res.status(200).json(students);
+});
+ 
+app.get("/api/students/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const student = students.find((st) => st.id === id);
+  if (!student) {
+    return res.status(404).json({ error: "Student not found." });
+  }
+  res.status(200).json(student);
+});
